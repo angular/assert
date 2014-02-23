441645cc8b9dab59e8eec556ac3f26670093dc59
Vojta Jina <vojta.jina@gmail.com>
2014-02-23 11:34:20 -0800

    docs: add comments for Docco

--- test/assert.spec.js
+++ test/assert.spec.js
@@ -1,7 +1,25 @@
+// # Assert.js
+// A run-time type assertion library for JavaScript. Designed to be used with [Traceur](https://github.com/google/traceur-compiler).
+
+
+// - [Basic Type Check](#basic-type-check)
+// - [Custom Check](#custom-check)
+// - [Primitive Values](#primitive-values)
+// - [Describing more complex types](#describing-more-complex-types)
+//   - [assert.arrayOf](#assert-arrayof)
+//   - [assert.structure](#assert-structure)
+// - [Integrating with Traceur](#integrating-with-traceur)
+
 import {assert} from 'assert';
 
 
 
+// ## Basic Type Check
+// By default, `instanceof` is used to check the type.
+//
+// Note, you can use `assert.type()` when unit testing or anywhere in your code.
+// However, most of the time, you will use it with Traceur.
+// Jump to the [Traceur section](#integrating-with-traceur) to see an example of that.
 describe('basic type check', function() {
 
   class Type {}
@@ -18,10 +36,17 @@ describe('basic type check', function() {
 });
 
 
+
+// ## Custom Check
+// Often, `instanceof` is not flexible enough.
+// In that case, your type can define its own `assert` method, which will be used instead.
+//
+// See define" for examples how to define custom checks, using the `assert.define()`.
 describe('custom check', function() {
 
   class Type {}
 
+  // the basic check can just return true/false, without specifying any reason
   it('should pass when returns true', function() {
     Type.assert = function(value) {
       return true;
@@ -41,6 +66,7 @@ describe('custom check', function() {
   });
 
 
+  // Using `assert.fail()` allows to report even multiple errors.
   it('should fail when calls assert.fail()', function() {
     Type.assert = function(value) {
       assert.fail('not smart enough');
@@ -67,6 +93,12 @@ describe('custom check', function() {
 });
 
 
+
+// ## Primitive Values
+// You don't wanna check primitive values such as strings, numbers or booleans with `instanceof`.
+// These get check using `typeof`.
+//
+// Again, you probably won't write this code and rather use Traceur to do it for you, simply based on type annotations.
 describe('primitive value check', function() {
 
   describe('string', function() {
@@ -113,8 +145,18 @@ describe('primitive value check', function() {
 });
 
 
+
+// ## Describing more complex types
+//
+// Often, a simple type check using `instanceof` or `typeof` is not enough.
+// That's why you can define custom checks using this DSL.
+// The goal was to make them easy to compose and as descriptive as possible.
+// Of course you can write your own DSL on the top of this.
 describe('define', function() {
 
+  // If the first argument to `assert.define()` is a type (function), it will define `assert` method on that function.
+  //
+  // In this example, being a type of Type means being a either a function or object.
   it('should define assert for an existing type', function() {
     class Type {}
 
@@ -131,6 +173,8 @@ describe('define', function() {
   });
 
 
+  // If the first argument to `assert.define()` is a string,
+  // it will create an interface - basically an empty class with `assert` method.
   it('should define an interface', function() {
     var User = assert.define('MyUser', function(user) {
       assert(user).is(Object);
@@ -143,6 +187,11 @@ describe('define', function() {
   });
 
 
+  // Here are a couple of more APIs to describe your custom types...
+  //
+  // ### assert.arrayOf
+  // Checks if the value is an array and if so, it checks whether all the items are one the given types.
+  // Note, these types can be composed types, not just simple ones.
   describe('arrayOf', function() {
 
     var Titles = assert.define('ListOfTitles', function(value) {
@@ -172,6 +221,9 @@ describe('define', function() {
   });
 
 
+  // ### assert.structure
+  // Similar to `assert.arrayOf` which checks a content of an array,
+  // `assert.structure` checks if the value is an object with specific properties.
   describe('structure', function() {
 
     var User = assert.define('MyUser', function(value) {
@@ -204,6 +256,17 @@ describe('define', function() {
 });
 
 
+
+// ## Integrating with Traceur
+//
+// Most of the time, you won't put `assert.type()` calls in your code and rather have Traceur to do it for you,
+// based on the type annotations.
+//
+// This has several advantages:
+// - it's shorter and nicer,
+// - you can easily ignore it when generating production code.
+//
+// Note, you need to run Traceur with `--types=true --type-assertions=true --type-assertion-module="path/to/assert"`.
 describe('Traceur', function() {
 
   describe('arguments', function() {
@@ -257,3 +320,8 @@ describe('Traceur', function() {
     });
   });
 });
+
+
+// <center><small>
+// This documentation has been generated from [assert.spec.js](https://github.com/vojtajina/assert/blob/master/test/assert.spec.js), using [Docco](http://jashkenas.github.io/docco/).
+// </small></center>
