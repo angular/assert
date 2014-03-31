e54233210c71e6324fc8f2b90082932a25afdb5a
Vojta Jina <vojta.jina@gmail.com>
2014-03-31 14:43:46 -0700

    chore: set up es6 build

--- gulpfile.js
+++ gulpfile.js
@@ -21,7 +21,13 @@ gulp.task('build_source_cjs', function() {
       .pipe(gulp.dest('dist/cjs'));
 });
 
-gulp.task('build_dist', ['build_source_cjs', 'build_source_amd']);
+gulp.task('build_source_es6', function() {
+  gulp.src(path.src)
+      .pipe(traceur(pipe.traceur({outputLanguage: 'es6'})))
+      .pipe(gulp.dest('dist/es6'));
+});
+
+gulp.task('build_dist', ['build_source_cjs', 'build_source_amd', 'build_source_es6']);
 gulp.task('build', ['build_dist']);
 
 
